08cdaddd2aabc8df4b594790819c52f5fbb69a49
lvyafei <lvyafeixx@gmail.com>
2016-08-21 18:07:16 +0800

    增加框架底层实现

---
+++ src/main/java/com/fs/platform/commons/annotation/FeSaService.java
@@ -0,0 +1,15 @@
+package com.fs.platform.commons.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface FeSaService {
+
+	String value() default "";
+}
